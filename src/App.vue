74167f5239c2f9db193986eeb8f0626676c86085
MJoshua25 <46876900+MJoshua25@users.noreply.github.com>
2020-11-24 12:45:46 +0000

    Import rest of components

--- src/App.vue
+++ src/App.vue
@@ -12,10 +12,14 @@
 
 <script>
 import TheHeader from './components/TheHeader.vue'
+import BadgeList from './components/BadgeList.vue'
+import UserInfo from './components/UserInfo.vue'
 
 export default {
   components:{
-    TheHeader
+    TheHeader,
+    UserInfo,
+    BadgeList
   },
   data() {
     return {
